f74c9bcdb1322e30b92b47dd0e723f56f47cc9a9
hackleberry <griffinschreiber@gmail.com>
2025-02-08 14:25:10 +0000

    Working triangle!

--- src/main.c
+++ src/main.c
@@ -23,7 +23,26 @@ void error_callback_glfw(int error, const char *msg) {
   exit(1);
 }
 
+// need debug printf function
+
+// initialize glfw
+// intialize window
+// load gl functions with glad
+// load shaders
+// main loop
+// poll events
+// handle keypresses <-- game stuff happens here
+// handle resize
+// clear color and depth buffer
+// tell shaders data <-- game stuff happens here
+// use shaders
+// swap buffers
+// destroy window
+// terminate glfw
+
+
 int main() {
+  // initialize glfw
   printf("Starting GLFW %s. \n", glfwGetVersionString());
 
   glfwSetErrorCallback(error_callback_glfw);
@@ -40,6 +59,7 @@ int main() {
 
   glfwWindowHint(GLFW_SAMPLES, 8);
 
+  // intialize window
   GLFWmonitor *monitor = NULL;
 
   int win_w = 800;
@@ -53,6 +73,7 @@ int main() {
                                         );
   glfwMakeContextCurrent(window);
 
+  // load gl functions with glad
   int glad_version = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
   if (glad_version == 0) {
     fprintf(stderr, "ERROR: Failed to initialize OpenGL context.\n");
@@ -62,22 +83,76 @@ int main() {
   printf("Renderer: %s.\n", glGetString(GL_RENDERER));
   printf("OpenGL version supported %s.\n", glGetString(GL_VERSION));
 
+  // initialize vbos
+  float points[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+  };
+
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+  // initialize vao
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  // load shaders
+  const char *vert_shader = read_file("src/shaders/main.vert");
+  const char *frag_shader = read_file("src/shaders/main.frag");
+
+  /* printf("DEBUG: vertex shader: \n%s\n", vert_shader); */
+  /* printf("DEBUG: fragment shader: \n%s\n", frag_shader); */
+
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vert_shader, NULL);
+  glCompileShader(vs);
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &frag_shader, NULL);
+  glCompileShader(fs);
+
+  GLuint shader_program = glCreateProgram();
+  glAttachShader(shader_program, fs);
+  glAttachShader(shader_program, vs);
+  glLinkProgram(shader_program);
+
+  // main loop
   while (!glfwWindowShouldClose(window)) {
+    // poll events
     glfwPollEvents();
+    // handle keypresses
     if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
       glfwSetWindowShouldClose(window, 1);
     }
 
+    // handle resize
     glfwGetFramebufferSize(window, &win_w, &win_h);
     glViewport(0, 0, win_w, win_h);
 
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    // clear color and depth buffer
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    // put the shader program and vao in the focus of the opengl state machine
+    glUseProgram(shader_program);
+    glBindVertexArray(vao);
+
+    // draw
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // swap buffers
     glfwSwapBuffers(window);
   }
 
+  // destroy window
   glfwDestroyWindow(window);
+  // terminate glfw
   glfwTerminate();
 
   return 0;
--- src/shaders/main.vert
+++ src/shaders/main.vert
@@ -1,10 +1,5 @@
 #version 330 core
 in vec3 vertex_position;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
 void main() {
-     gl_Position = projection * view * model * vec4(vertex_position, 1.0);
+     gl_Position = vec4(vertex_position, 1.0);
 }
